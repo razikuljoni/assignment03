9bdbbfc5e99834f272513fa64a57526488f325d8
Md. Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2021-01-22 15:41:31 +0600

    soved all problems

--- assignment.js
+++ assignment.js
@@ -9,6 +9,9 @@
 // var result = kilometerToMeter(11);
 // console.log(result);
 
+
+
+
 //budgetCalculator
 
 // function budgetCalculator(watch,phone,laptop) {
@@ -20,18 +23,23 @@
 // var total = budgetCalculator(2, 1, 2);
 // console.log(total);
 
+
+
+
 //hotelCost
 
 // function hotelCost(day) {
 //     var cost = 0;
 //     if (day <= 10) {
 //         cost = day * 100;
-//     } else if (day <= 20) {
+//     }
+//     else if (day <= 20) {
 //         var firstPart = 10 * 100;
 //         var remainingDays = day - 10;
 //         var secondPart = remainingDays * 80;
 //         cost = firstPart + secondPart;
-//     } else {
+//     }
+//     else {
 //         var firstPart = 10 * 100;
 //         var secondPart = 10 * 80;
 //         var remainingDays = day - 20;
@@ -40,10 +48,29 @@
 //     }
 //     return cost;
 // }
-// var totalCost = hotelCost(17);
+// var totalCost = hotelCost(15);
 // console.log(totalCost);
 
 
 
 //megaFriend
 
+function megaFriend(arr) {
+    var lgth = 0;
+    var longest;
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].length > lgth) {
+            var lgth = arr[i].length;
+            longest = arr[i];
+        }
+    }
+    return longest;
+}
+var longest = megaFriend([
+    "joni",
+    "second item",
+    "third longish item",
+    "third longish item one",
+]);
+console.log(longest);
